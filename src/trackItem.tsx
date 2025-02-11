88d75520051bef3ce85b5b102d0629511d6c6dca
jyecs <gmding777@gmail.com>
2025-02-10 21:13:10 -0800

    Changed the item list format, added image to it

--- src/trackItem.tsx
+++ src/trackItem.tsx
@@ -9,10 +9,12 @@ const TrackItem:React.FC<Props> = ( {track} ) => {
     const imgSrc = track.album.images[0].url;
 
 
-    return (<li className="text-center flex flex-row items-center justify-between mb-3 ml-5 mr-5 border border-solid border-white p-3">
-        <img className="max-w-20 max-h-20" src={imgSrc}></img>
-        <span>{name}</span>
-        <span>{artist}</span>
+    return (<li className="text-center flex flex-row items-center mb-3 ml-5 mr-5 border border-solid border-white p-3 hover:bg-gray-600">
+        <img className="max-w-20 max-h-20 mr-5" src={imgSrc}></img>
+        <div className="flex flex-col text-left">  
+            <div>{name}</div>
+            <div>{artist}</div>
+        </div>
     </li>)
 }
 

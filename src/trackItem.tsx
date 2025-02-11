a9d9c589f723d60f6df84f367afaa1726d268180
jyecs <gmding777@gmail.com>
2025-02-10 20:44:17 -0800

    Fixed some react types/interface to mirror the one that the vite one is using

--- src/trackItem.tsx
+++ src/trackItem.tsx
@@ -6,8 +6,11 @@ const TrackItem:React.FC<Props> = ( {track} ) => {
 
     const name = track.name;
     const artist = track.artists[0].name;
+    const imgSrc = track.album.images[0].url;
+
 
     return (<li className="text-center flex flex-row items-center justify-between mb-3 ml-5 mr-5 border border-solid border-white p-3">
+        <img className="max-w-20 max-h-20" src={imgSrc}></img>
         <span>{name}</span>
         <span>{artist}</span>
     </li>)
--- src/types.d.ts
+++ src/types.d.ts
@@ -36,6 +36,29 @@ interface Track {
     is_local: boolean;
 }
 
+interface Album {
+    album_type: string;
+    total_tracks: number;
+    available_markets: Array<string>;
+    external_urls: object;
+    href: string;
+    id: string;
+    images: Array<ImageObject>;
+    name: string;
+    release_date: string;
+    release_date_precision: string;
+    restrictions: object;
+    type: string;
+    uri: string;
+    artists: Array<SimplifiedArtistObject>
+}
+
+interface ImageObject {
+    url: string;
+    height: number;
+    width: number;
+}
+
 interface SimplifiedArtistObject {
     external_urls: object;
     href: string;
@@ -43,4 +66,62 @@ interface SimplifiedArtistObject {
     name: string;
     type: string;
     uri: string;
+}
+
+interface ArtistObject {
+    external_urls: object;
+    followers: object;
+    genres: string[];
+    href: string;
+    id: string;
+    images: object[];
+    name: string;
+    popularity: number;
+    type: string;
+    uri: string;
+}
+
+interface FetchedArtists {
+    artists: ArtistObject[];
+}
+
+interface SpotifyPlaylist {
+    collaborative: boolean;
+    description: string | null;
+    external_urls: object;
+    followers: object;
+    href: string;
+    id: string;
+    images: object; // change this
+    name: string;
+    owner: object; // change this
+    public: boolean;
+    snapshot_id: string;
+    tracks: object; // hopefully don't need to change this
+    type: string;
+    uri: string
+}
+
+interface UserProfile {
+    country: string;
+    display_name: string;
+    email: string;
+    explicit_content: {
+        filter_enabled: boolean,
+        filter_locked: boolean
+    },
+    external_urls: { spotify: string; };
+    followers: { href: string; total: number; };
+    href: string;
+    id: string;
+    images: Image[];
+    product: string;
+    type: string;
+    uri: string;
+}
+
+interface Image {
+    url: string;
+    height: number;
+    width: number;
 }

12360c8e4c8952fc25b6805a8da4c244cc7c92a1
jyecs <gmding777@gmail.com>
2025-02-13 14:59:32 -0800

    fixed some css styling so things are placed where they are supposed to be

--- src/tracksModal.tsx
+++ src/tracksModal.tsx
@@ -10,12 +10,12 @@ const TracksModal:React.FC<Props> = ( {isOpen, closeModal, tracks }) => {
     if (!isOpen) { return null }
 
     return (
-    <div className="z-10 inset-0 absolute text-black text-lg bg-white m-10 flex flex-col h-100% w-100%">
-        <div className="flex flex-row h-10 self-center w-100">
-            <div className="text-white bg-gray-700">All Songs</div>
-            <button onClick={closeModal} className="text-red-700 hover:text-gray-700 mr-2 self-end justify-self-end">X</button>
+    <div className="flex flex-col z-10 inset-0 absolute text-black text-lg bg-gray-500 m-10 pb-3">
+        <div className="h-10 w-full">
+            <div className="text-white bg-gray-500 place-center text-center">All Songs</div>
+            <button onClick={closeModal} className="text-red-700 hover:text-gray-600 mr-2 absolute top-0 right-0">X</button>
         </div>
-        <div className="overflow-auto">
+        <div className="overflow-auto bg-gray-500">
             <ul>
                 {tracks.map((track, index) => {
                     return <TrackItem track={track} key={index}></TrackItem>

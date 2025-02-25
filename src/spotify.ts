0d74dad00263b555a789c11096440a18e14c2736
jyecs <gmding777@gmail.com>
2025-02-24 23:06:51 -0800

    Changed Redirect URI in another place

--- src/spotify.ts
+++ src/spotify.ts
@@ -45,7 +45,7 @@ async function getAccessToken(code: string): Promise<string> {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://listify-3aydnlyyf-jyecs-projects.vercel.app/");
     params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
